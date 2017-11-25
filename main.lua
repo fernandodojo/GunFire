13b669d556d5cf4d54bea36222303b62c3874034
Fernando Henrique <fernandodojo@gmail.com>
2017-11-25 15:26:44 -0300

    Fully functional circle circle collision

--- main.lua
+++ main.lua
@@ -14,16 +14,13 @@ function love.load()
   	circle.x = 300
   	circle.y= 300
   	circle.r = 10
+  	speed2 = 0
 
-  	circle2 = {}
-  	circle2.x = 300
-  	circle2.y= 300
-  	circle2.r = 10
-
-  	   	
+  	  	   	
 end
 
 function love.update(dt)
+	speed2 = 0
 				
 	if love.keyboard.isDown("a") and player.x  > 0 then		
 		player.x = player.x - player.speed * dt
@@ -37,13 +34,30 @@ function love.update(dt)
 	if love.keyboard.isDown("s") and player.y < 600 then
 		player.y = player.y + player.speed * dt			
 	end
+
+	if circlecolision(circle.x, circle.y, player.x, player.y, player.r + circle.r) then
+		love.graphics.print("colisao", 100,200)
+		if player.x < circle.x then	 -- colisão entre circulo e circulo			
+			player.x = player.x -2
+			player.y = player.y -2
+		elseif player.x > circle.x then
+			player.x = player.x +2
+			player.y = player.y -2
+	    end
+	    if player.y < circle.y then
+			player.y = player.y -2
+	    elseif player.y > circle.y then
+			player.y = player.y -2		
+	    end
+	end
 end
 	
+
 
 function love.draw()
 	love.graphics.circle("fill", player.x, player.y, player.r)
 	love.graphics.circle("line", circle.x, circle.y, circle.r)
-	if circlecolision(circle.x, circle.y, player.x, player.y, player.r + circle.r) then	 -- colisão entre circulo e quadrado
+	if circlecolision(circle.x, circle.y, player.x, player.y, player.r + circle.r +2) then	 -- colisão entre circulo e quadrado
 		love.graphics.print("colisao", 100,200)
 	end	
 end
