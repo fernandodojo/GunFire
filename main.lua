de841a1c9ad2f2ce0b54619d3a2ae847bee5d3b9
Fernando Henrique <fernandodojo@gmail.com>
2017-12-05 10:43:55 -0300

    #

--- main.lua
+++ main.lua
@@ -1,6 +1,4 @@
---https://stackoverflow.com/questions/21089959/detecting-collision-of-rectangle-with-circle
 require "squarecolission"
-require "circlecolision"
 
 function love.load()
 	player = {}
@@ -8,121 +6,64 @@ function love.load()
 	player.y = 50
 	player.speed = 100
 	
-  	
 
-  	retangulo ={}
-  	retangulo.x = 300
-  	retangulo.y= 300
-
+	x= 300
+	y = 300
   	h = 25
   	w = 100
 
+  	speedw = player.speed
+  	speeda = player.speed
+  	speeds = player.speed
+  	speedd = player.speed
 
-   	
+
+ 	
 end
 
 function love.update(dt)
-	local speed = 0
-	local speed2 = player.speed
-	if squarecolission(player.x, player.y, retangulo.x, retangulo.y, w, h ) then
-		speed2 = 0
-	end
-			
-	if love.keyboard.isDown("a") and player.x  > 0 then		
-		player.x = player.x - speed2 * dt
-	end
-	if love.keyboard.isDown("d") and player.x  < 800  then
-		player.x = player.x + player.speed * dt
-	end
-	if love.keyboard.isDown("w") and player.y > 0 then
-		player.y = player.y - (player.speed + 200) * dt
-	end
-	if love.keyboard.isDown("s") and player.y < 600 then
-		player.y = player.y + speed2 * dt			
-	end
 	
 
-	b = (retangulo.x + w/2) - player.x
-	c = (retangulo.y + h/2) - player.y
-	if ( b < 10 + w/2 and b > (10 + w/2)*-1) and (c < 10 + h/2 and c > (10 + h/2)* -1) then
-		player.x = player.x -2
+	if love.keyboard.isDown("a") and player.x  > 0 then		
+		player.x = player.x - speeda * dt
+	end
+	if love.keyboard.isDown("d") and player.x  < 800  then
+		player.x = player.x + speedd * dt
+	end
+	if love.keyboard.isDown("w") and player.y > 0 then
+		player.y = player.y - speedw * dt
+	end
+	if love.keyboard.isDown("s") and player.y < 600 then
+		player.y = player.y + speeds * dt			
 	end
 end
 
 function love.draw()
-	love.graphics.circle("fill", player.x, player.y, 10, 6)
-	love.graphics.rectangle("line", retangulo.x, retangulo.y, w, h)
-	love.graphics.print(b, 100, 100)
-	if ( b < 10 + w/2 and b > (10 + w/2)*-1) and (c < 10 + h/2 and c > (10 + h/2)* -1) then	 -- colisão entre circulo e quadrado
-		love.graphics.print("colisao", 100,200)
-	end	
-	if squarecolission(player.x, player.y, retangulo.x, retangulo.y, w, h ) then
+	love.graphics.circle("fill", player.x, player.y, 10)	
+	love.graphics.rectangle("line", x, y, w, h)
+	if squarecolission(player.x, player.y, 10, x, y, w, h ) then
 		love.graphics.print("ok", 0, 50)
-	end
-end
-
-
-
---[[
-
-require "squarecolission"
-
-function love.load()
-	player = {}
-	player.x =  350
-	player.y = 50
-	player.speed = 100
-	
-
-  	h = 25
-  	w = 100
-
-
-  	terreno = {}
-  	for i= 100, 400 do
-  		terreno[i] = {}
-  		for j = 300, 325 do
-  			terreno[i][j] = 1
-  		end
-  	end 	
-end
-
-function love.update(dt)
-	local speed = 0
-	local speed2 = player.speed
-	for i= 100, 400, 100 do
-  		for j = 300, 400, 25 do
-  			if terreno[i][j] == 1 then
-  				if squarecolission(player.x, player.y, i, j, w, h ) then
-					speed2 = 0
-				end
-			end  			
-  		end
-  	end
-
-	if love.keyboard.isDown("a") and player.x  > 0 then		
-		player.x = player.x - player.speed * dt
-	end
-	if love.keyboard.isDown("d") and player.x  < 800  then
-		player.x = player.x + player.speed * dt
-	end
-	if love.keyboard.isDown("w") and player.y > 0 then
-		player.y = player.y - (player.speed + 200) * dt
-	end
-	if love.keyboard.isDown("s") and player.y < 600 then
-		player.y = player.y + speed2 * dt			
-	end
-end
-
-function love.draw()
-	love.graphics.circle("fill", player.x, player.y, 10)
-	for i= 100, 400, 100 do
-  		for j = 300, 325, 25 do
-		    love.graphics.rectangle("line", i, j, w, h)
-		    if squarecolission(player.x, player.y, i, j, w, h ) then
-				love.graphics.print("ok", 0, 50)
-			end
+		if player.x< x and player.y> y then
+			speedd = 0
+			speedw = player.speed					
 		end
+		if player.x> x then
+			speeda = 0
+			speedw = player.speed
+		end				
+		if player.y< y then
+			speeds = 0
+			speeda = player.speed						
+		end
+		--if player.y> y then
+			--speedw = 0
+			-- = player.speed			
+		--end
+		
+	else
+		speedw = player.speed
+	  	speeda = player.speed
+	  	speeds = player.speed
+	  	speedd = player.speed		
 	end
 end
-]]
