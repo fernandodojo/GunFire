0587a8e5d80b20f15ea75ba1712cbf3a79c25a59
Fernando Henrique <fernandodojo@gmail.com>
2017-12-28 21:36:54 -0300

    allowing the player to move on the new floor. Zoom on each moviment.

--- src/moviments.lua
+++ src/moviments.lua
@@ -19,33 +19,37 @@ end
 
 function moviments_update(dt)
 	
-	if love.keyboard.isDown("a") and player1.x > 15 and motionlimiter1 >0  and gamestate == "player1" then
+	if love.keyboard.isDown("a") and player1.x > -300 and motionlimiter1 >0  and gamestate == "player1" then
 		player1.x = player1.x - player1.speed * dt
 		motionlimiter1 = motionlimiter1 - player1.speed * dt
 		if play and noise then
 			movimentsound:play()
 		end
+		gameraflag1 = true
 	end
-	if love.keyboard.isDown("d") and player1.x < 785 and motionlimiter1 >0 and gamestate == "player1" then
+	if love.keyboard.isDown("d") and player1.x < 1050 and motionlimiter1 >0 and gamestate == "player1" then
 		player1.x = player1.x + player1.speed * dt
 		motionlimiter1 = motionlimiter1 - player1.speed * dt
 		if play and noise then
 			movimentsound:play()
 		end
+		gameraflag1 = true
 	end
-	if love.keyboard.isDown("kp4") and player2.x > 15 and motionlimiter2 >0 and gamestate == "player2"then
+	if love.keyboard.isDown("kp4") and player2.x > -300 and motionlimiter2 >0 and gamestate == "player2"then
 		player2.x = player2.x - player2.speed * dt
 		motionlimiter2 = motionlimiter2 - player1.speed * dt
 		if play and noise then
 			movimentsound:play()
 		end
+		gameraflag2 = true
 	end
-	if love.keyboard.isDown("kp6") and player2.x < 785 and motionlimiter2 >0 and gamestate == "player2"then
+	if love.keyboard.isDown("kp6") and player2.x < 1050 and motionlimiter2 >0 and gamestate == "player2"then
 		player2.x = player2.x + player2.speed * dt
 		motionlimiter2 = motionlimiter2 - player1.speed * dt
 		if play and noise then
 			movimentsound:play()
 		end
+		gameraflag2 = true
 	end
 
 end
