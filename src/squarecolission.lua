af1782cb389f59dcfb4bb4a1f60e4f1de72a8ea5
Fernando Henrique <fernandodojo@gmail.com>
2018-01-08 00:43:01 -0300

    removing old functions

--- src/squarecolission.lua
+++
@@ -1,5 +0,0 @@
-function squarecolission(xt, yt,raio, x, y, width, height )
-	return  (xt + raio >= x and xt + raio <= x + width)  and  (yt + raio >= y and yt + raio <= y + height) or (xt - raio >= x and xt - raio <= x + width)  and  (yt - raio >= y and yt - raio <= y + height) or (xt + raio >= x and xt + raio <= x + width)  and  (yt - raio >= y and yt - raio <= y + height) or (xt - raio >= x and xt - raio <= x + width)  and  (yt + raio >= y and yt + raio <= y + height)
-end
-
---função checa toque entre retangulos e quadrados
